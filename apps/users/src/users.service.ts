dbb49cc79575573deb65dd04520780f92a7aa8e2
Nondukishor <nipuchakraborty86@hotmail.com>
2023-06-14 00:04:53 +0600

    update

--- apps/users/src/users.service.ts
+++ apps/users/src/users.service.ts
@@ -4,7 +4,18 @@ import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  private readonly users: User[] = [];
+  private readonly users: User[] = [
+    {
+      id: "1",
+      email: "example@gmail.com",
+      password: "1234"
+    },
+    {
+      id: "2",
+      email: "example2@gmail.com",
+      password: "1234"
+    }
+  ];
 
   create(createUserInput: CreateUserInput) {
     this.users.push(createUserInput);
